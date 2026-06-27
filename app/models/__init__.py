be3108a73fab0b6d74899aae3875b213e12e8873
Thushanika <thushanikaparamalingam02@gmail.com>
2026-06-27 13:21:22 +0530

    Changes made

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
 from app.models.user_model import User
 from app.models.customer_model import Customer
+from app.models.seller_model import Seller
 
-__all__ = ["User", "Customer"]
+__all__ = ["User", "Customer", "Seller"]
 
--- app/routes/customer_routes.py
+++ app/routes/customer_routes.py
@@ -1,25 +1,21 @@
 from flask import Blueprint
-from flask_jwt_extended import jwt_required
 from app.controllers import customer_controller as ctrl
 
 customer_bp = Blueprint("", __name__, url_prefix="/api/customers")
 
 
 @customer_bp.route("", methods=["POST"])
-@jwt_required()
 def create_customer():
     return ctrl.create_customer()
 
 
 
 @customer_bp.route("", methods=["GET"])
-@jwt_required()
 def get_customers():
     return ctrl.get_customers()
 
 
 @customer_bp.route("/<int:user_id>", methods=["GET"])
-@jwt_required()
 def get_customer(user_id):
     return ctrl.get_customer(user_id)
 
@@ -30,7 +26,6 @@ def update_customer(user_id):
 
 
 @customer_bp.route("/<int:user_id>", methods=["DELETE"])
-
 def delete_customer(user_id):
     return ctrl.delete_customer(user_id)
 
