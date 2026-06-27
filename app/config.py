b19650eead32c610e8f348a212e9f269c1ace6d5
Shanujan <shanushanujan234@gmail.com>
2026-06-27 11:37:57 +0530

    config.py updated

--- app/config.py
+++ app/config.py
@@ -11,7 +11,7 @@ class Config:
     DB_NAME = os.getenv("DB_NAME")
     DB_USER = os.getenv("DB_USER")
 
-    SQLALCHEMY_DATABASES_URI = (
+    SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
